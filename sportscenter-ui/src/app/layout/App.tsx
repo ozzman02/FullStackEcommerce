d42a77b41b95bc108460a1e529ea8ed684bc9bf2
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-11-28 23:12:32 -0600

    frontend - implementing react router

--- sportscenter-ui/src/app/layout/App.tsx
+++ sportscenter-ui/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import Catalog from "../../features/catalog/Catalog"
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
 
@@ -27,7 +27,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container sx={{ paddingTop: "64px" }}>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   )
--- sportscenter-ui/src/app/router/Routes.tsx
+++ sportscenter-ui/src/app/router/Routes.tsx
@@ -3,6 +3,7 @@ import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import Catalog from "../../features/catalog/Catalog";
 import ContactPage from "../../features/contact/ContactPage";
+import ProductDetails from "../../features/catalog/ProductDetails";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
         children: [
             { path: '', element: <HomePage /> },
             { path: 'store', element: <Catalog /> },
+            { path: 'store/:id', element: <ProductDetails /> },
             { path: 'contact', element: <ContactPage /> }
         ]
     }
