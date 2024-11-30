25f82f95730ce735c0c910bebb0d8b62352b3d88
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-11-30 16:14:51 -0600

    backend - exception handling

---
+++ sportscenter-api/src/main/java/com/ecommerce/sportscenter/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ecommerce.sportscenter.exceptions;
+
+import com.ecommerce.sportscenter.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<?> productNotFoundExceptionHandler(ProductNotFoundException exception, WebRequest webRequest) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .error("Product does not exist")
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
